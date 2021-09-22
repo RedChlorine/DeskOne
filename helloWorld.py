a7c65fe3a0955dd0717fb2912a13ba753134b47b
RedChlorine <91183494+RedChlorine@users.noreply.github.com>
2021-09-22 14:52:11 +0200

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,19 @@
+print("Hello World!!!")
+
+#print and operators
+print("OwO") #strings = str. they are always in ""
+print("//" * 10) #str followed by opperator
+
+#Variables
+price = 50 #identifyer
+print("R" + str(price)) #str()is a modifier or converter
+print("//"*10)
+
+
+#The age app
+print("Hi therer! I will tell you your birth year.")
+print("/////"*10)
+currentYear = input("What is the current year? ")
+theirAge = input("How old are you?" )
+birthYear = int(currentYear) - int(theirAge) #modified the input str to integers = int().
+print("You were born in " + str(birthYear)+ "!")
